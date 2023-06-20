bedc4bd685052859f843aa4633be9bef289b5c1d
tejb96 <104933640+tejb96@users.noreply.github.com>
2023-06-19 18:33:00 -0600

    line plot

--- time_series_visualizer.py
+++ time_series_visualizer.py
@@ -5,37 +5,51 @@ from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
 
 # Import data (Make sure to parse dates. Consider setting index column to 'date'.)
-df = None
+df = pd.read_csv('fcc-forum-pageviews.csv', index_col='date', parse_dates=True)
+# print(df)
 
 # Clean data
-df = None
+top_p = df.value.quantile(0.975)
+lower_p = df.value.quantile(0.025)
+# print(df<lower_p)
+df = df.mask((df< lower_p) | (df > top_p))
+df = df.dropna(subset='value')
+# print(df.columns)
 
 
 def draw_line_plot():
     # Draw line plot
-
-
-
-
-
+    fig=plt.figure()
+    plt.plot(df.index, df['value'],color='red')
+    plt.title('Daily freeCodeCamp Forum Page Views 5/2016-12/2019')
+    plt.xlabel('Date')
+    plt.ylabel('Page Views')
+    plt.show()
+   
     # Save image and return fig (don't change this part)
     fig.savefig('line_plot.png')
     return fig
 
 def draw_bar_plot():
     # Copy and modify data for monthly bar plot
-    df_bar = None
+    df_bar = df.copy()
+    df_bar['month'] = df_bar.index.month
+    df_bar['year'] = df_bar.index.year
+    df_bar = df_bar.groupby(['year','month']).mean()   
+    print(df_bar)
 
     # Draw bar plot
-
-
-
-
+    fig=plt.figure()
+    plt.bar(df_bar.index.year, df_bar.value, label=df_bar.index.month) 
+    plt.xlabel("Courses offered")
+    plt.ylabel("No. of students enrolled")
+    plt.title("Students enrolled in different courses")
+    plt.show()
 
     # Save image and return fig (don't change this part)
     fig.savefig('bar_plot.png')
     return fig
-
+draw_bar_plot()
 def draw_box_plot():
     # Prepare data for box plots (this part is done!)
     df_box = df.copy()
